f5c7375aa3602bd5b0de9623e162ca4fc94f3566
sayan-adhikary <pap9679429830@gmail.com>
2025-07-11 11:07:09 +0530

    Updated

---
+++ 07_operators/01_arithmetic_operators.py
@@ -0,0 +1,12 @@
+a = 34
+
+b = 2 
+
+# Arithmetic Operator 
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a / b = ", a / b)
+print("a % b = ", a % b)
+print("a // b = ", a // b)  #Floor division operator it returns the largest integer less than or equal to the division result
+print("a ** b = ", a ** b)  #Exponentiation operator its works like a^b 
---
+++ 07_operators/03_logical_operators.py
@@ -0,0 +1,18 @@
+c = True 
+d = False
+
+# Logical Operators
+print(True and True)    # True if both operands are true
+print(True and False)   # False if one operand is false
+print(False and True)   # False if one operand is false
+print(False and False)  # False if both operands are false
+
+print("For Or Operator...")
+print(True or True) # True if at least one operand is true
+print(True or False)    # True if at least one operand is true
+print(False or True)    # True if at least one operand is true
+print(False or False)   # False if both operands are false
+
+print("Not Operator")
+print(not(True) )   
+print(not(False) ) 
