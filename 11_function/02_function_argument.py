680b35ab3ffb66c122972a7c5cf304e918a61c58
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2025-08-25 00:16:50 +0530

    updated

---
+++ 11_function/02_function_argument.py
@@ -0,0 +1,9 @@
+def add(a,b,pluse=0):
+    x=a+b+pluse
+    return x
+
+c=add(3,5,2)
+print(c)
+
+c1=add(b=2,a=3)
+print(c1)
---
+++ 11_function/04_recursion.py
@@ -0,0 +1,12 @@
+def fib(n):
+    #base case
+    if(n==0 or n==1):
+        return n
+    return fib(n-2)+fib(n-1)
+print(fib(6))
+
+fib(6)
+fib(4)+fib(5)
+fib(2)+fib(3)+fib(3)+fib(4)
+fib(0)+fib(1)+fib(1)+fib(2)+fib(1)+fib(2)+fib(2)+fib(3)
+0+1+1+fib(0)+fib()
